97f10cb25efb097b7ba920a8d1466ea35a3c13dc
Shamil Ablyazov <ablyazov.shamil@gmail.com>
2016-03-17 23:56:28 +0300

    add nullable to column

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,8 +6,8 @@ from django.utils.timezone import now
 class Question(models.Model):
     title = models.CharField(max_length=1024)
     text = models.TextField()
-    added_at = models.DateTimeField(default=now)
-    rating = models.IntegerField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(blank=True)
     author = models.ForeignKey(User, related_name='author')
     likes = models.ManyToManyField(User, related_name="likes")
 
