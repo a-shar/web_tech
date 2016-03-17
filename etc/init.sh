601c2f191b8ff541a747a276631d4115d03a47e2
Shamil Ablyazov <ablyazov.shamil@gmail.com>
2016-03-17 23:02:50 +0300

    add mysql init

--- etc/init.sh
+++ etc/init.sh
@@ -1,2 +1,9 @@
 rm /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
+
+
+sudo /etc/init.d/mysql restart
+mysql -uroot -e "create database myproject;"
+mysql -uroot -e "CREATE USER 'enth'@'localhost' IDENTIFIED BY 'password';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'enth'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
